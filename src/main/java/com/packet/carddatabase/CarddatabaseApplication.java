b82f775662272fd1e5f1fd1ac7945f82127dee8f
Dylan Balagtas <dylansatgalab@gmail.com>
2025-07-31 18:13:04 +0800

    inserts records into table and prints car brands

--- src/main/java/com/packet/carddatabase/CarddatabaseApplication.java
+++ src/main/java/com/packet/carddatabase/CarddatabaseApplication.java
@@ -1,19 +1,35 @@
 package com.packet.carddatabase;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.packet.carddatabase.domain.CarRepository;
+import com.packet.carddatabase.domain.Car;
+
 @SpringBootApplication
-public class CarddatabaseApplication {
+public class CarddatabaseApplication implements CommandLineRunner{
 	
 	private static final Logger logger = LoggerFactory.getLogger(CarddatabaseApplication.class);
+	
+	@Autowired
+	private CarRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarddatabaseApplication.class, args);
-		logger.info("Application started");
+		
+	}
+	@Override
+	public void run(String...args) throws Exception {
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000));
+		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000));
+		for (Car car: repository.findAll()) {
+			logger.info(car.getBrand() + " " + car.getModel());
+		}
+		
 	}
 
 }
